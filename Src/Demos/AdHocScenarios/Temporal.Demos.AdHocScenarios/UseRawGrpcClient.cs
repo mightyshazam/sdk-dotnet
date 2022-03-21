d555eac0af7508498ba47f2f3cf7bdf608ccfdb1
macrogreg <macrogreg@live.com>
2022-03-21 10:48:59 -0700

    Add workflow outcome waiting to demo.

--- Src/Demos/AdHocScenarios/Temporal.Demos.AdHocScenarios/UseRawGrpcClient.cs
+++ Src/Demos/AdHocScenarios/Temporal.Demos.AdHocScenarios/UseRawGrpcClient.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using Candidly.Util;
+using Google.Protobuf;
 using Grpc.Core;
 using Temporal.Api.Common.V1;
+using Temporal.Api.Enums.V1;
+using Temporal.Api.History.V1;
 using Temporal.Api.TaskQueue.V1;
 using Temporal.Api.Workflow.V1;
 using Temporal.Api.WorkflowService.V1;
@@ -21,8 +25,11 @@ namespace Temporal.Demos.AdHocScenarios
 
         public void Run()
         {
+            Console.WriteLine();
+
             ListWorkflowExecutionsAsync().GetAwaiter().GetResult();
-            RunAsync().GetAwaiter().GetResult();
+            StartWorkflowAsync().GetAwaiter().GetResult();
+            WaitForWorkflowAsync().GetAwaiter().GetResult();
 
             Console.WriteLine();
             Console.WriteLine("Done. Press Enter.");
@@ -44,12 +51,12 @@ namespace Temporal.Demos.AdHocScenarios
         private WorkflowService.WorkflowServiceClient CreateClientNetCore()
         {
 #if NETCOREAPP
-            if (!RuntimeEnvironmentInfo.SingeltonInstance.CoreAssembyInfo.IsSysPrivCoreLib)
+            if (!RuntimeEnvironmentInfo.SingletonInstance.CoreAssembyInfo.IsSysPrivCoreLib)
             {
                 throw new InvalidOperationException("RuntimeEnvironmentInfo.SingeltonInstance.CoreAssembyInfo.IsSysPrivCoreLib was expected to be True.");
             }
 
-            if (RuntimeEnvironmentInfo.SingeltonInstance.RuntimeVersion.StartsWith("3"))
+            if (RuntimeEnvironmentInfo.SingletonInstance.RuntimeVersion.StartsWith("3"))
             {
                 AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             }
@@ -75,15 +82,14 @@ namespace Temporal.Demos.AdHocScenarios
 
         private WorkflowService.WorkflowServiceClient CreateClient()
         {
-            return RuntimeEnvironmentInfo.SingeltonInstance.CoreAssembyInfo.IsMscorlib
+            return RuntimeEnvironmentInfo.SingletonInstance.CoreAssembyInfo.IsMscorlib
                         ? CreateClientNetFx()
                         : CreateClientNetCore();
         }
 
         public async Task ListWorkflowExecutionsAsync()
         {
-            Console.WriteLine();
-            Console.WriteLine("----------- Workflow Executions { ----------- -----------");
+            Console.WriteLine("\n----------- Workflow ListWorkflowExecutionsAsync { ----------- -----------\n");
 
             WorkflowService.WorkflowServiceClient client = CreateClient();
 
@@ -102,12 +108,14 @@ namespace Temporal.Demos.AdHocScenarios
                                 + $" Status=\"{weInfo.Status}\"");
             }
 
-            Console.WriteLine("----------- } Workflow Executions ----------- -----------");
+            Console.WriteLine();
+            Console.WriteLine("----------- } Workflow ListWorkflowExecutionsAsync ----------- -----------");
+            Console.WriteLine();
         }
 
-        public async Task RunAsync()
+        public async Task StartWorkflowAsync()
         {
-            Console.WriteLine();
+            Console.WriteLine("\n----------- Workflow StartWorkflowAsync { ----------- -----------\n");
 
             WorkflowService.WorkflowServiceClient client = CreateClient();
 
@@ -137,6 +145,174 @@ namespace Temporal.Demos.AdHocScenarios
             {
                 Console.WriteLine($"Could not start Workflow Execution. {rpcEx}");
             }
+
+            Console.WriteLine("\n----------- } Workflow StartWorkflowAsync ----------- -----------\n");
+        }
+
+        public class UnexpectedServerResponseException : Exception
+        {
+            private static string FormatMessage(string serverCall, string scenario, string problemDescription)
+            {
+                StringBuilder message = new();
+
+                if (!String.IsNullOrWhiteSpace(problemDescription))
+                {
+                    message.Append(problemDescription);
+                }
+
+                if (!String.IsNullOrWhiteSpace(problemDescription))
+                {
+                    if (message.Length > 0 && message[message.Length - 1] != '.')
+                    {
+                        if (message[message.Length - 1] != '.')
+                        {
+                            message.Append('.');
+                        }
+
+                        message.Append(' ');
+                    }
+
+                    message.Append("Server Call: \"");
+                    message.Append(serverCall);
+                    message.Append("\".");
+                }
+
+                if (!String.IsNullOrWhiteSpace(scenario))
+                {
+                    if (message.Length > 0 && message[message.Length - 1] != '.')
+                    {
+                        if (message[message.Length - 1] != '.')
+                        {
+                            message.Append('.');
+                        }
+
+                        message.Append(' ');
+                    }
+
+                    message.Append("Scenario: \"");
+                    message.Append(scenario);
+                    message.Append("\".");
+                }
+
+                return message.ToString();
+            }
+
+            public UnexpectedServerResponseException(string serverCall, string scenario, string problemDescription)
+                : base(FormatMessage(serverCall, scenario, problemDescription))
+            {
+            }
+
+            public UnexpectedServerResponseException(string serverCall, string scenario, Exception unexpectedException)
+                : base(FormatMessage(serverCall, scenario, $"Unexpected exception occurred ({unexpectedException.GetType().Name})"))
+            {
+            }
+        }
+
+        public async Task WaitForWorkflowAsync()
+        {
+            const string DemoWorkflowId = "Some-Workflow-Id";
+
+            Console.WriteLine();
+            Console.WriteLine("\n----------- Workflow WaitForWorkflowAsync { ----------- -----------\n");
+
+            try
+            {
+                await WaitForWorkflowAsync(DemoWorkflowId, workflowRunId: String.Empty);
+                Console.WriteLine($"Workflow result received successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting workflow result. {ex}");
+            }
+
+            Console.WriteLine("\n----------- } Workflow WaitForWorkflowAsync ----------- -----------\n");
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0010:Add missing cases", Justification = "Only need to process terminal events.")]
+        private async Task WaitForWorkflowAsync(string workflowId, string workflowRunId)
+        {
+            WorkflowService.WorkflowServiceClient client = CreateClient();
+
+            const string ServerCallDescription = "GetWorkflowExecutionHistoryAsync(..) with HistoryEventFilterType = CloseEvent";
+            ByteString nextPageToken = ByteString.Empty;
+
+            // Spin and retry or follow the workflow chain until we get a result, or hit a non-retriable error, or time out:
+            while (true)
+            {
+
+                GetWorkflowExecutionHistoryRequest reqGWEHist = new()
+                {
+                    Namespace = "default",
+                    Execution = new WorkflowExecution()
+                    {
+                        WorkflowId = workflowId,
+                        RunId = workflowRunId
+                    },
+                    NextPageToken = nextPageToken,
+                    WaitNewEvent = true,
+                    HistoryEventFilterType = HistoryEventFilterType.CloseEvent,
+                };
+
+                GetWorkflowExecutionHistoryResponse resGWEHist = await client.GetWorkflowExecutionHistoryAsync(reqGWEHist);
+
+                if (resGWEHist.History.Events.Count == 0 && resGWEHist.NextPageToken != null && resGWEHist.NextPageToken.Length > 0)
+                {
+                    Console.WriteLine("Received no history events, but a non-empty NextPageToken. Repeating call.");
+                    nextPageToken = resGWEHist.NextPageToken;
+                    continue;
+                }
+
+                if (resGWEHist.History.Events.Count != 1)
+                {
+                    throw new UnexpectedServerResponseException(ServerCallDescription,
+                                                                nameof(WaitForWorkflowAsync),
+                                                                $"History.Events.Count was expected to be 1, but it was in fact {resGWEHist.History.Events.Count}");
+                }
+
+                HistoryEvent historyEvent = resGWEHist.History.Events[0];
+
+                switch (historyEvent.EventType)
+                {
+                    case EventType.WorkflowExecutionCompleted:
+                        Console.WriteLine("Workflow completed succsessfully.");
+                        return;
+
+                    case EventType.WorkflowExecutionFailed:
+                        Console.WriteLine($"Workflow execution failed ({historyEvent.WorkflowExecutionFailedEventAttributes.Failure.Message}).");
+                        return;
+
+                    case EventType.WorkflowExecutionTimedOut:
+                        Console.WriteLine($"Workflow execution timed out.");
+                        return;
+
+                    case EventType.WorkflowExecutionCanceled:
+                        Console.WriteLine($"Workflow execution cancelled.");
+                        return;
+
+                    case EventType.WorkflowExecutionTerminated:
+                        Console.WriteLine($"Workflow execution terminated (reason=\"{historyEvent.WorkflowExecutionTerminatedEventAttributes.Reason}\").");
+                        return;
+
+                    case EventType.WorkflowExecutionContinuedAsNew:
+                        string nextRunId = historyEvent.WorkflowExecutionContinuedAsNewEventAttributes.NewExecutionRunId;
+
+                        if (String.IsNullOrWhiteSpace(nextRunId))
+                        {
+                            throw new UnexpectedServerResponseException(ServerCallDescription,
+                                                                        nameof(WaitForWorkflowAsync),
+                                                                        $"EventType was WorkflowExecutionContinuedAsNew, but NewExecutionRunId=\"{nextRunId}\"");
+                        }
+
+                        Console.WriteLine($"Workflow execution continued as new. Following workflow chain to RunId=\"{nextRunId}\"");
+                        workflowRunId = nextRunId;
+                        continue;
+
+                    default:
+                        throw new UnexpectedServerResponseException(ServerCallDescription,
+                                                                    nameof(WaitForWorkflowAsync),
+                                                                    $"Unexpected History EventType (\"{historyEvent.EventType}\")");
+                }
+            }  // while(true)
         }
     }
 }
