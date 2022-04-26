06a459c8ea7e387fe2fcf90110fd9878376d0b0e
macrogreg <macrogreg@live.com>
2022-04-26 12:43:47 -0700

    Set up the `Temporal.Sdk.WorkflowClient.UsageSamples` project.

---
+++ Src/Samples/WorkflowClient.UsageSamples/Temporal.Sdk.WorkflowClient.UsageSamples/public/Part1_SimpleClientUsage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Temporal.Sdk.WorkflowClient.UsageSamples
+{
+    public class Part1_SimpleClientUsage
+    {
+        public void Run()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}.{nameof(Run)}(..) started.\n");
+
+            RunAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine();
+            Console.WriteLine($"\n{this.GetType().Name}.{nameof(Run)}(..) finished.\nPress Enter.");
+            Console.ReadLine();
+        }
+
+        public async Task RunAsync()
+        {
+            Console.WriteLine($"\n{this.GetType().Name}.{nameof(RunAsync)}(..) started.\n");
+
+            Console.WriteLine($"\n{this.GetType().Name}.{nameof(RunAsync)}(..) finished.\n");
+        }
+    }
+}
---
+++ Src/Samples/WorkflowClient.UsageSamples/Temporal.Sdk.WorkflowClient.UsageSamples/public/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Temporal.Util;
+
+namespace Temporal.Sdk.WorkflowClient.UsageSamples
+{
+    public class Program
+    {
+        public static void Main(string[] _)
+        {
+            Console.WriteLine($"RuntimeEnvironmentInfo: \n{RuntimeEnvironmentInfo.SingletonInstance}");
+
+            (new Part1_SimpleClientUsage()).Run();
+
+            Console.WriteLine($"\n{typeof(Program).FullName} has finished.\n");
+        }
+    }
+}
