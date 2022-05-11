360e1864474777d7ae94718c87e0255dafdce082
macrogreg <macrogreg@live.com>
2022-05-11 14:48:13 -0700

    Add comments to clerify the implementation of `WorkflowServiceClientFactory`.

--- Src/SDK/WorkflowClient/Temporal.WorkflowClient/internal/WorkflowServiceClientEnvelope.cs
+++ Src/SDK/WorkflowClient/Temporal.WorkflowClient/internal/WorkflowServiceClientEnvelope.cs
@@ -16,8 +16,10 @@ namespace Temporal.WorkflowClient
         private readonly ChannelBase _grpcChannel;
         private readonly TemporalClientConfiguration.Connection _connectionConfig;
 
-        private readonly object _refCountLock = new();  // Held for very short periods and relatively seldom.
+        // The the detailed deascription of the `WorkflowServiceClientFactory` class for context on why we are
+        // doing ref-counting here, instead of just relying on the GC.
 
+        private readonly object _refCountLock = new();  // Held for very short periods and relatively seldom.
         private int _refCount = 0;
 
         public WorkflowServiceClientEnvelope(WorkflowService.WorkflowServiceClient grpcWorkflowServiceClient)
