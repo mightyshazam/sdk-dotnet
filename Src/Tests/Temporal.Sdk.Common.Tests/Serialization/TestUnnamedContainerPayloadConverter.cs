b9bd4b560ecdd9a2b6a02b607706a02b8adc0498
KyJah Keys <kyjah@awarehq.com>
2022-05-12 17:36:25 -0400

    removed var

--- Src/Tests/Temporal.Sdk.Common.Tests/Serialization/TestUnnamedContainerPayloadConverter.cs
+++ Src/Tests/Temporal.Sdk.Common.Tests/Serialization/TestUnnamedContainerPayloadConverter.cs
@@ -12,8 +12,13 @@ namespace Temporal.Sdk.Common.Tests.Serialization
         [Fact]
         public void Test_UnnamedContainerPayloadConverter_TrySerialize_String()
         {
+<<<<<<< Updated upstream
+            UnnamedContainerPayloadConverter instance = new UnnamedContainerPayloadConverter();
+            Payloads p = new Payloads();
+=======
             var instance = new UnnamedContainerPayloadConverter();
             var p = new Payloads();
+>>>>>>> Stashed changes
             Assert.False(instance.TrySerialize(String.Empty, p));
             Assert.Empty(p.Payloads_);
         }
@@ -21,8 +26,13 @@ namespace Temporal.Sdk.Common.Tests.Serialization
         [Fact]
         public void Test_UnnamedContainerPayloadConverter_TrySerialize_Null()
         {
+<<<<<<< Updated upstream
+            UnnamedContainerPayloadConverter instance = new UnnamedContainerPayloadConverter();
+            Payloads p = new Payloads();
+=======
             var instance = new UnnamedContainerPayloadConverter();
             var p = new Payloads();
+>>>>>>> Stashed changes
             Assert.False(instance.TrySerialize<string>(null, p));
             Assert.Empty(p.Payloads_);
         }
@@ -30,10 +40,11 @@ namespace Temporal.Sdk.Common.Tests.Serialization
         [Fact]
         public void Test_UnnamedContainerPayloadConverter_TrySerialize_Unnamed_SerializedDataBacked()
         {
-            var instance = new UnnamedContainerPayloadConverter();
+<<<<<<< Updated upstream
+            UnnamedContainerPayloadConverter instance = new UnnamedContainerPayloadConverter();
             instance.InitDelegates(new[]{new JsonPayloadConverter()});
-            var p = new Payloads();
-            var data = new PayloadContainers.Unnamed.SerializedDataBacked(new Payloads
+            Payloads p = new Payloads();
+            PayloadContainers.Unnamed.SerializedDataBacked data = new PayloadContainers.Unnamed.SerializedDataBacked(new Payloads
             {
                 Payloads_ =
                 {
@@ -43,6 +54,14 @@ namespace Temporal.Sdk.Common.Tests.Serialization
                     }
                 }
             }, new JsonPayloadConverter());
+=======
+            var p = new Payloads();
+            var converter = new NewtonsoftJsonPayloadConverter();
+            converter.Serialize(new SerializableClass { Name = "test", Value = 2 }, p);
+            var instance = new UnnamedContainerPayloadConverter();
+            instance.InitDelegates(new[]{converter});
+            var data = new PayloadContainers.Unnamed.SerializedDataBacked(p, converter);
+>>>>>>> Stashed changes
             Assert.True(instance.TrySerialize(data, p));
             Assert.NotEmpty(p.Payloads_);
             Assert.True(instance.TryDeserialize(p, out PayloadContainers.Unnamed.SerializedDataBacked cl));
@@ -55,10 +74,17 @@ namespace Temporal.Sdk.Common.Tests.Serialization
         [Fact]
         public void Test_UnnamedContainerPayloadConverter_TrySerialize_Unnamed_InstanceBacked()
         {
+<<<<<<< Updated upstream
+            UnnamedContainerPayloadConverter instance = new UnnamedContainerPayloadConverter();
+            instance.InitDelegates(new[]{new JsonPayloadConverter()});
+            Payloads p = new Payloads();
+            PayloadContainers.Unnamed.InstanceBacked<string> data = new PayloadContainers.Unnamed.InstanceBacked<string>(new[]{"hello"});
+=======
             var instance = new UnnamedContainerPayloadConverter();
             instance.InitDelegates(new[]{new JsonPayloadConverter()});
             var p = new Payloads();
             var data = new PayloadContainers.Unnamed.InstanceBacked<string>(new[]{"hello"});
+>>>>>>> Stashed changes
             Assert.True(instance.TrySerialize(data, p));
             Assert.NotEmpty(p.Payloads_);
             Assert.False(instance.TryDeserialize(p, out PayloadContainers.Unnamed.InstanceBacked<string> _));
@@ -71,10 +97,17 @@ namespace Temporal.Sdk.Common.Tests.Serialization
         [Fact]
         public void Test_UnnamedContainerPayloadConverter_TrySerialize_Unnamed_Empty()
         {
+<<<<<<< Updated upstream
+            UnnamedContainerPayloadConverter instance = new UnnamedContainerPayloadConverter();
+            instance.InitDelegates(new[]{new JsonPayloadConverter()});
+            Payloads p = new Payloads();
+            PayloadContainers.Unnamed.Empty data = new PayloadContainers.Unnamed.Empty();
+=======
             var instance = new UnnamedContainerPayloadConverter();
             instance.InitDelegates(new[]{new JsonPayloadConverter()});
             var p = new Payloads();
             var data = new PayloadContainers.Unnamed.Empty();
+>>>>>>> Stashed changes
             Assert.True(instance.TrySerialize(data, p));
             Assert.Empty(p.Payloads_);
         }
