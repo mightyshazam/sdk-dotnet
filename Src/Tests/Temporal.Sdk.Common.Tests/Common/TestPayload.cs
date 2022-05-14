d3e499822856b2e61e4b54f38a38584703df7d8e
KyJah Keys <kyjah@awarehq.com>
2022-05-13 21:15:30 -0400

    updated namespaces in TestPayload to shorten type names

--- Src/Tests/Temporal.Sdk.Common.Tests/Common/TestPayload.cs
+++ Src/Tests/Temporal.Sdk.Common.Tests/Common/TestPayload.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Temporal.Common;
+using Temporal.Common.Payloads;
 using Xunit;
 
 namespace Temporal.Sdk.Common.Tests
@@ -17,14 +18,14 @@ namespace Temporal.Sdk.Common.Tests
         [Fact]
         public void Test_Payload_Unnamed_With_Variadic_Arguments()
         {
-            Temporal.Common.Payloads.PayloadContainers.Unnamed.InstanceBacked<object> payload = Payload.Unnamed(new object(), new object());
+            PayloadContainers.Unnamed.InstanceBacked<object> payload = Payload.Unnamed(new object(), new object());
             AssertUnnamedCorrectness(2, payload);
         }
 
         [Fact]
         public void Test_Payload_Unnamed_With_Array_Arguments()
         {
-            Temporal.Common.Payloads.PayloadContainers.Unnamed.InstanceBacked<int> payload = Payload.Unnamed(new[] { 1, 2, 3 });
+            PayloadContainers.Unnamed.InstanceBacked<int> payload = Payload.Unnamed(new[] { 1, 2, 3 });
             AssertUnnamedCorrectness(3, payload);
         }
 
@@ -32,7 +33,7 @@ namespace Temporal.Sdk.Common.Tests
         public void Test_Payload_Unnamed_With_Enumerable_Arguments()
         {
             int length = 10;
-            Temporal.Common.Payloads.PayloadContainers.Unnamed.InstanceBacked<string> payload = Payload.Unnamed(Enumerable.Repeat("hello", length));
+            PayloadContainers.Unnamed.InstanceBacked<string> payload = Payload.Unnamed(Enumerable.Repeat("hello", length));
             AssertUnnamedCorrectness(length, payload);
         }
 
@@ -41,16 +42,16 @@ namespace Temporal.Sdk.Common.Tests
         {
             int length = 10;
             IReadOnlyList<string> lst = Enumerable.Repeat("hello", length).ToList();
-            Temporal.Common.Payloads.PayloadContainers.Unnamed.InstanceBacked<string> payload = Payload.Unnamed(lst);
+            PayloadContainers.Unnamed.InstanceBacked<string> payload = Payload.Unnamed(lst);
             AssertUnnamedCorrectness(length, payload);
         }
 
         private static void AssertUnnamedCorrectness<T>(
             int length,
-            Temporal.Common.Payloads.PayloadContainers.Unnamed.InstanceBacked<T> payload)
+            PayloadContainers.Unnamed.InstanceBacked<T> payload)
         {
             Assert.Equal(length, payload.Count);
-            foreach (Temporal.Common.Payloads.PayloadContainers.UnnamedEntry entry in payload.Values)
+            foreach (PayloadContainers.UnnamedEntry entry in payload.Values)
             {
                 T value = entry.GetValue<T>();
                 Assert.IsType<T>(value);
